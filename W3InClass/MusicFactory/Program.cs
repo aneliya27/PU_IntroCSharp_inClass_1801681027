6c39d9f7593f45241ac8c20f8b9ca4d944b51de0
aneliya27 <ani.belcheva@abv.bg>
2018-10-16 15:41:44 +0300

    blablalbla

--- W3InClass/MusicFactory/Program.cs
+++ W3InClass/MusicFactory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,33 @@ namespace MusicFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Play some music...");
+            Console.WriteLine("Do you want to play some music?");
+            string answer=Console.ReadLine();
+            Console.WriteLine($"My answer is: {answer}");
+            if (answer == "Yes")
+            {
+                Console.WriteLine("Play music");
+
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\1b_A\PU_IntroCSharp_inClass_1801681027\W3InClass\MusicFactory\Music\music.mp3"))
+                {
+
+                    player.Play();
+
+                }
+            }
+            else if (answer == "No")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else if (answer == "Fuck")
+            {
+                Console.WriteLine("Yes, fuck you too!!!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+
         }
     }
 }
